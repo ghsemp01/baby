0b1cb91757cb6348ee9428e53f92fc3b62163af1
Jayson Temp <iloveprogramming17@gmail.com>
2017-10-30 17:10:26 +0800

    init

---
+++ index.php
@@ -0,0 +1,121 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="description" content="">
+    <title>Baby <3</title>
+  
+   
+  
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+  </head>
+  <body class="parallax-demo">
+
+<main>
+<!--  Parallax Section  -->
+  <div class="parallax-container">
+    <div class="parallax"><img src="img/1.jpg"></div>
+  </div>
+  <div class="section white">
+    <div class="row container">
+      <h2 class="header"></h2>
+		<blockquote class="grey-text text-darken-3 lighten-3">
+			When I first saw you, I like you, but not quite <br>
+			We're strangers, we're kissing, am I holding you that tight?<br>
+			Those eyes, those lips, am I falling? Well I might.<br>
+			Are you too? Say yes and I'll keep you, all my life.<br>
+		</blockquote>
+    </div>
+  </div>
+  <div class="parallax-container">
+    <div class="parallax"><img src="img/2.jpg"></div>
+  </div>
+<div class="section white">
+    <div class="row container">
+      <h2 class="header"></h2>
+      <blockquote class="grey-text text-darken-3 lighten-3">
+		We decided to go to a beautiful place, <br>
+		I just know you for a week, I can't comprehend the phase.<br>
+		My heart beats faster as we talk face to face.<br>
+		That's when I realise you're the girl I wanna chase.<br>
+	  </blockquote>
+    </div>
+  </div>
+    <div class="parallax-container">
+    <div class="parallax"><img src="img/3.jpg"></div>
+  </div>
+<div class="section white">
+    <div class="row container">
+      <h2 class="header"></h2>
+      <p class="grey-text text-darken-3 lighten-3">
+	    <blockquote class="grey-text text-darken-3 lighten-3">
+		We decided to go to a beautiful place, <br>
+		I just know you for a week, I can't comprehend the phase.<br>
+		My heart beats faster as we talk face to face.<br>
+		That's when I realise you're the girl I wanna chase.<br>
+		</blockquote>
+	  </p>
+    </div>
+  </div>
+     <div class="parallax-container">
+    <div class="parallax"><img src="img/4.jpg"></div>
+  </div>
+	<div class="section white">
+    <div class="row container">
+      <h2 class="header"></h2>
+       <blockquote class="grey-text text-darken-3 lighten-3">
+		We decided to go to a beautiful place, <br>
+		I just know you for a week, I can't comprehend the phase.<br>
+		My heart beats faster as we talk face to face.<br>
+		That's when I realise you're the girl I wanna chase.<br>
+		</blockquote>
+    </div>
+  </div>
+     <div class="parallax-container">
+    <div class="parallax"><img src="img/5.jpg"></div>
+  </div>
+<div class="section white">
+    <div class="row container">
+      <h2 class="header"></h2>
+       <blockquote class="grey-text text-darken-3 lighten-3">
+		We decided to go to a beautiful place, <br>
+		I just know you for a week, I can't comprehend the phase.<br>
+		My heart beats faster as we talk face to face.<br>
+		That's when I realise you're the girl I wanna chase.<br>
+		</blockquote>
+		</div>
+  </div>
+     <div class="parallax-container">
+    <div class="parallax"><img src="img/6.jpg"></div>
+  </div>
+<div class="section white">
+    <div class="row container">
+      <h2 class="header"></h2>
+		<blockquote class="grey-text text-darken-3 lighten-3">
+		We decided to go to a beautiful place, <br>
+		I just know you for a week, I can't comprehend the phase.<br>
+		My heart beats faster as we talk face to face.<br>
+		That's when I realise you're the girl I wanna chase.<br>
+		</blockquote>
+		</div>
+  </div>
+<!-- end paralax -->
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+      
+	<script>
+		   $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+        
+	</script>
+  </body>
+</html>
